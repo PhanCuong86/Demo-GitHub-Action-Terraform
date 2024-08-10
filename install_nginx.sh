16278cd6842aa5527a090c66d640bdc88198ac50
unknown <cuongxuanphan9999@gmail.com>
2024-08-10 13:22:35 +0700

    test nginx

--- install_nginx.sh
+++ install_nginx.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
-sudo apt-get update
-sudo apt-get install nginx -y
+sudo su
+sudo yum update -y
+sudo yum install nginx -y
+sudo systemctl start nginx
+#sudo systemctl enable nginx
+#sudo systemctl status nginx
+
+#sudo apt-get update
+#sudo apt-get install nginx -y
 echo "<h1>Hi DC1-This is Github Action Pipeline</h1>" | sudo tee /var/www/html/index.nginx-debian.html
